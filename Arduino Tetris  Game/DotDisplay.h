3d8c03713b81f91a139cfed51068ac049379d20c
Nat Weerawan <nat.wrw@gmail.com>
2014-07-21 09:51:12 +0700

    Apply with command "astyle  --style=allman"

--- Arduino Tetris  Game/DotDisplay.h
+++ Arduino Tetris  Game/DotDisplay.h
@@ -1,23 +1,24 @@
 #include "Arduino.h"
 
 
-class DotDisplay {
-   public:
-      DotDisplay();
-      void Initialize() ;
-      void fn_display(boolean (*data)[32][16]);
+class DotDisplay
+{
+public:
+    DotDisplay();
+    void Initialize() ;
+    void fn_display(boolean (*data)[32][16]);
 
-   private:      
-      byte led;
-      byte led_set;
-      byte sin1;
-      byte sin2;
-      byte sin3;
-      byte clk;
-      byte lat;
-      byte str;
-      boolean compare2; 
-      boolean compare1; 
+private:
+    byte led;
+    byte led_set;
+    byte sin1;
+    byte sin2;
+    byte sin3;
+    byte clk;
+    byte lat;
+    byte str;
+    boolean compare2;
+    boolean compare1;
 };
 
 
--- Arduino Tetris  Game/Hero.h
+++ Arduino Tetris  Game/Hero.h
@@ -1,85 +1,86 @@
 #include "Arduino.h"
 
 
-class Hero {
-   public:
-      Hero();
-      byte type;
-      byte heroRotate;
-      
-      short posX;
-      short posY;
+class Hero
+{
+public:
+    Hero();
+    byte type;
+    byte heroRotate;
 
-      float timeCount;
-      float timeCMax;
-      
-      boolean keyDownL1;
-      boolean keyDownSpace;
-      boolean keyDownR1;
-      
-      boolean keyDownLeft;
-      boolean keyDownDown;
-      boolean keyDownRight;
-      
-      boolean keyDownReset;
-      boolean keyDownPause;
-      boolean keyDownLightUp;  
+    short posX;
+    short posY;
 
-      boolean keyDownLevelDown;
-      boolean keyDownLevelUp;
-      boolean keyDownLightDown;    
-      
-      //keyDownDown   
-      boolean holdKeyL1;
-      boolean holdKeySpace;
-      boolean holdKeyR1;
-      
-      boolean holdKeyLeft;
-      boolean holdKeyDown;
-      boolean holdKeyRight;
-      
-      boolean holdKeyReset;
-      boolean holdKeyPause;
-      boolean holdKeyLightUp;  
+    float timeCount;
+    float timeCMax;
 
-      boolean holdKeyLevelDown;
-      boolean holdKeyLevelUp;
-      boolean holdKeyLightDown;  
+    boolean keyDownL1;
+    boolean keyDownSpace;
+    boolean keyDownR1;
 
-      float timeRMoveCount;
-      float timeLMoveCount;
-      float timePauseCount;
-      float SpeedMove;
-      float SpeedDown;
-      float SpeedLevel;
-      
-      short buffRRotate;
-      short buffLRotate;
-      
-      float SpeedLevelNormal;
-      void Update(byte *gameStep,float *deltaTime,boolean (*data)[32][16],boolean (*data2)[36][16],const boolean (*tetris)[7][4][4][4]);
-      void DispalyUpdate(boolean (*data)[32][16],boolean (*data2)[36][16],const boolean (*tetris)[7][4][4][4]);
-      
-      void GamePause(byte *gameStep,float *deltaTime,boolean (*data)[32][16],boolean (*data2)[36][16],const boolean (*tetris)[7][4][4][4]);
-      void GameReset(byte *gameStep,float *deltaTime,boolean (*data)[32][16],boolean (*data2)[36][16],const boolean (*tetris)[7][4][4][4]);
+    boolean keyDownLeft;
+    boolean keyDownDown;
+    boolean keyDownRight;
+
+    boolean keyDownReset;
+    boolean keyDownPause;
+    boolean keyDownLightUp;
+
+    boolean keyDownLevelDown;
+    boolean keyDownLevelUp;
+    boolean keyDownLightDown;
+
+    //keyDownDown
+    boolean holdKeyL1;
+    boolean holdKeySpace;
+    boolean holdKeyR1;
+
+    boolean holdKeyLeft;
+    boolean holdKeyDown;
+    boolean holdKeyRight;
+
+    boolean holdKeyReset;
+    boolean holdKeyPause;
+    boolean holdKeyLightUp;
+
+    boolean holdKeyLevelDown;
+    boolean holdKeyLevelUp;
+    boolean holdKeyLightDown;
+
+    float timeRMoveCount;
+    float timeLMoveCount;
+    float timePauseCount;
+    float SpeedMove;
+    float SpeedDown;
+    float SpeedLevel;
+
+    short buffRRotate;
+    short buffLRotate;
+
+    float SpeedLevelNormal;
+    void Update(byte *gameStep,float *deltaTime,boolean (*data)[32][16],boolean (*data2)[36][16],const boolean (*tetris)[7][4][4][4]);
+    void DispalyUpdate(boolean (*data)[32][16],boolean (*data2)[36][16],const boolean (*tetris)[7][4][4][4]);
+
+    void GamePause(byte *gameStep,float *deltaTime,boolean (*data)[32][16],boolean (*data2)[36][16],const boolean (*tetris)[7][4][4][4]);
+    void GameReset(byte *gameStep,float *deltaTime,boolean (*data)[32][16],boolean (*data2)[36][16],const boolean (*tetris)[7][4][4][4]);
     /*
       void Initialize() ;
       void fn_display(boolean (*data)[32][16]);
       void DispalyUpdate(boolean (*data)[32][16],boolean (*data2)[36][16],const boolean (*tetris)[7][4][4][4]);
-	  */
-   private:    
-	   /*
-      byte led;
-      byte led_set;
-      byte sin1;
-      byte sin2;
-      byte sin3;
-      byte clk;
-      byte lat;
-      byte str;
-      boolean compare2; 
-      boolean compare1; 
-	  */
+      */
+private:
+    /*
+    byte led;
+    byte led_set;
+    byte sin1;
+    byte sin2;
+    byte sin3;
+    byte clk;
+    byte lat;
+    byte str;
+    boolean compare2;
+    boolean compare1;
+    */
 };
 
 
